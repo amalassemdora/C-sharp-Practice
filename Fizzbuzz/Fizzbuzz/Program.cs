5c76f9b6cb4c1d94972a49e3046a331e6600792f
Amal Assem <amalassem21@gmail.com>
2021-03-18 20:55:36 +0200

    Initial Commit

---
+++ Fizzbuzz/Fizzbuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Fizzbuzz
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int n = 1; n <= 100; n++)
+			{
+				if (n % 3 == 0 && n % 5 == 0)
+				{
+					Console.Write("fizzbuzz");
+				}
+				else if (n % 3 == 0)
+				{
+					Console.Write("fizz");
+				}
+				else if (n % 5 == 0)
+				{
+					Console.Write("buzz");
+				}
+				else
+				{
+					Console.Write(n);
+				}
+				while (n < 100)
+				{
+					Console.Write(", ");
+				}
+				Console.ReadKey();
+			}
+		}
+	}
+}
